6b14ba5a3cc0bd893211e2537d719f74c829341e
G.Santhosh <49428544+SanthoshGa@users.noreply.github.com>
2019-04-25 09:55:17 +0530

    Update Utility.java

--- algorithms/Utility.java
+++ algorithms/Utility.java
@@ -241,6 +241,91 @@ public static String[] bubbleSortForString(String[] arr)
 	return arr;
 	
 }
+public static void twoDArray()
+{
+	System.out.println("Enter 1 for integer array");
+	System.out.println("Enter 2 for double array");
+	System.out.println("Enter 3 for boolean array");
+	int choice = Utility.getNumber();
+	switch (choice)
+	{
+		case 1:
+				System.out.println("Enter rows of array");
+				int rows = Utility.getNumber();
+				System.out.println("Enter coloums of array");
+				int coloums = Utility.getNumber();
+				int arr[][] = new int[rows][coloums];
+				System.out.println("Enter your array");
+				for (int i=0;i<arr.length;i++)
+				{
+					for (int j=0;j<arr.length;j++)
+					{
+						arr[i][j] = Utility.getNumber();
+					}
+				}
+				for (int i=0;i<arr.length;i++)
+				{
+					for (int j=0;j<arr.length;j++)
+					{
+						System.out.print(arr[i][j]+" ");
+					}
+					System.out.println();
+				}
+				System.out.println();
+				break;
+		case 2:
+				System.out.println("Enter rows of array");
+				int rowd = Utility.getNumber();
+				System.out.println("Enter coloums of array");
+				int coloumd = Utility.getNumber();
+				
+				double arrd[][] = new double[rowd][coloumd];
+				System.out.println("Enter your array");
+				for (int i=0;i<arrd.length;i++)
+				{
+					for (int j=0;j<arrd.length;j++)
+					{
+						arrd[i][j] = Utility.getDouble();
+					}
+				}
+				for (int i=0;i<arrd.length;i++)
+				{
+					for (int j=0;j<arrd.length;j++)
+					{
+						System.out.print(arrd[i][j]+" ");
+					}
+					System.out.println();
+				}
+				System.out.println();
+				break;
+			
+		case 3:
+				System.out.println("Enter rows of array: ");
+				int rowb = Utility.getNumber();
+				System.out.println("Enter coloums of array: ");
+				int coloumb = Utility.getNumber();
+				
+				boolean arrb[][] = new boolean[rowb][coloumb];
+				System.out.println("Enter your array");
+				for (int i=0;i<arrb.length;i++)
+				{
+					for (int j=0;j<arrb.length;j++)
+					{
+						arrb[i][j] = Utility.getBoolean();
+					}
+				}
+				for (int i=0;i<arrb.length;i++)
+				{
+					for (int j=0;j<arrb.length;j++)
+					{
+						System.out.print(arrb[i][j]+" ");
+					}
+					System.out.println();
+				}
+				System.out.println();
+				break;
+	}
+}
 public static int getNumber() 
 {
 	int n=sc.nextInt();
